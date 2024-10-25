17c1012e092ac163140081f16210cd786106efd6
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-25 14:09:59 +0530

    Initial commit

---
+++ src/main/java/com/weather_app/WeatherMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather_app/WeatherMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
